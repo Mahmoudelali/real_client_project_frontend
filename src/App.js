c25401196787e79c7a8fd109261484c72b5fe330
mahmoudElali <mahmud.elali98@gmail.com>
2023-04-20 10:26:20 +0300

    created the app route system

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// components
+import Home from './pages/home/Home.jsx';
+import Categories from './pages/category/Categories.jsx';
+import SubCategories from './pages/category/SubCategories.jsx';
+import ContactUs from './pages/contact-us/Contact-us.jsx';
+import Instruction from './pages/instruction/Instruction.jsx';
+import Orders from './pages/order/Orders.jsx';
+import Profile from './pages/Profile/Profile.jsx';
+
+import Dashboard from './pages/Dashboard/Dashboard.jsx';
+import Users from './pages/Dashboard/Users.jsx';
+import Admins from './pages/Dashboard/Admins.jsx';
+import Products from './pages/Dashboard/Products.jsx';
+import Settings from './pages/Dashboard/Settings.jsx';
+import AboutUs from './pages/about-us/About-us.jsx';
+import Cart from './pages/cart/Cart.jsx';
 
 function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				{/* so we have   ,orders , cart */}
+
+				<Routes>
+					{/* home routes */}
+					<Route path="/" element={<Home />}>
+						<Route path="/about" element={<AboutUs />} />
+						<Route path="/categories" element={<Categories />}>
+							<Route
+								path="/categories/category"
+								element={<SubCategories />}
+							/>
+						</Route>
+						<Route path="/contact" element={<ContactUs />} />
+						<Route path="/instructions" element={<Instruction />} />
+						<Route path="/orders" element={<Orders />} />
+						<Route path="/profile" element={<Profile />} />
+						<Route path="/cart" element={<Cart />} />
+					</Route>
+
+					{/* Dashboard routes */}
+					<Route path="/admin/dashboard" element={<Dashboard />}>
+						<Route
+							path="/admin/dashboard/settings"
+							element={<Settings />}
+						></Route>
+						<Route
+							path="/admin/dashboard/products"
+							element={<Products />}
+						></Route>
+						<Route
+							path="/admin/dashboard/admins"
+							element={<Admins />}
+						></Route>
+						<Route
+							path="/admin/dashboard/users"
+							element={<Users />}
+						></Route>
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/category/SubCategories.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SubCategories = () => {
+	return <div>sub categories</div>;
+};
+
+export default SubCategories;
