3e8cbee59cae6694104ce26569f229936fbba025
mahmoudElali <mahmud.elali98@gmail.com>
2023-04-20 13:44:41 +0300

    added collapsing side bar to the dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,105 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Outlet, NavLink } from 'react-router-dom';
+import { sidebarStatus } from '../../App.js';
+import './dashboard.css';
 
+// icons
+import { Grid } from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+
+import SettingsSharpIcon from '@mui/icons-material/SettingsSharp';
+
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import HomeSharpIcon from '@mui/icons-material/HomeSharp';
+import GroupIcon from '@mui/icons-material/Group';
+import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const sideLinks = [
+	{
+		icon: <HomeSharpIcon />,
+		path: '/admin/dashboard/home',
+		name: 'Home',
+	},
+	{
+		icon: <AdminPanelSettingsIcon />,
+		path: '/admin/dashboard/admins',
+		name: 'Admins',
+	},
+	{
+		icon: <GroupIcon />,
+		path: '/admin/dashboard/users',
+		name: 'Users',
+	},
+	{
+		icon: <ShoppingCartIcon />,
+		path: '/admin/dashboard/products',
+		name: 'Products',
+	},
+	{
+		icon: <SettingsSharpIcon />,
+		path: '/admin/dashboard/settings',
+		name: 'Settings',
+	},
+];
 const Dashboard = () => {
+	const [sidebarExpanded, setSidebarExpanded] = useContext(sidebarStatus);
+
 	return (
-		<div>
-			<h1>Dashboard</h1>
-			<Outlet />
+		<div className="dashboard-container">
+			<button
+				onClick={() => {
+					setSidebarExpanded(!sidebarExpanded);
+				}}
+				className="toggle-menu-icon"
+			>
+				<Grid item lg={1}>
+					{sidebarExpanded ? <CloseIcon /> : <MenuIcon />}
+				</Grid>
+			</button>
+			<div
+				className="side-bar-container"
+				style={{
+					width: sidebarExpanded ? '75%' : 0,
+					display: sidebarExpanded ? 'flex' : 'none',
+				}}
+			>
+				<div className="greetings">
+					<h1>Khizana</h1>
+					<h2>hello, name</h2>
+				</div>
+				<div className="side-links-container">
+					{sideLinks.map(({ icon, path, name }, index) => {
+						return (
+							<NavLink
+								key={index}
+								to={path}
+								onClick={() => {
+									setSidebarExpanded(!sidebarExpanded);
+								}}
+							>
+								<Grid item xs={1}>
+									{icon}
+								</Grid>
+								{name}
+							</NavLink>
+						);
+					})}
+				</div>
+				<NavLink
+					className={'logout-btn'}
+					to={'/admin/dashboard/logout'}
+				>
+					<Grid item xs={1}>
+						<LogoutIcon />
+					</Grid>
+					Logout
+				</NavLink>
+			</div>
+			<main>
+				<Outlet />
+			</main>
 		</div>
 	);
 };
