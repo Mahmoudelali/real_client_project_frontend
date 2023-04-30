04ab8b0d82e69afb683986984e8980c4f15ae948
mahmoudElali <mahmud.elali98@gmail.com>
2023-05-01 00:02:42 +0300

    "list admins ,promote/demote admins" ,delete admin

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import { Grid } from '@mui/material';
+import Cookies from 'js-cookie';
+import Switcher from './Switcher';
 var urlEnv = process.env.REACT_APP_URL;
 const User = ({
 	phone,
@@ -14,11 +16,18 @@ const User = ({
 	getAllUsers,
 	countryCallingCode,
 	adminsView,
-	
 }) => {
+	const [processing, setProcessing] = useState(false);
+	const [isSuper, setIsSuper] = useState(
+		role === 'superAdmin' ? true : false,
+	);
 	const handleDeleteUser = () => {
 		axios
-			.delete(`${urlEnv}/user/${_id}`)
+			.delete(`${urlEnv}/user/${_id}`, {
+				headers: {
+					auth_token: Cookies.get('auth_token'),
+				},
+			})
 			.then((res) => {
 				console.log(res);
 				getAllUsers();
@@ -34,6 +43,16 @@ const User = ({
 			<td style={{ color: 'GrayText' }}>
 				<a href={`mailto:${email}`}>{email}</a>
 			</td>
+			{adminsView && (
+				<td>
+					<Switcher
+						_id={_id}
+						setAdmin={true}
+						isSuper={isSuper}
+						setIsSuper={setIsSuper}
+					/>
+				</td>
+			)}
 			<td style={{ color: 'GrayText', textAlign: 'start' }}>
 				<span>
 					{role === 'admin'
@@ -49,22 +68,12 @@ const User = ({
 				<td style={{ color: 'GrayText' }}>+{countryCallingCode}</td>
 			)}
 
-			{adminsView && (
-				<td>
-					<button style={{ all: 'unset' }}>
-						<Grid x={1}>
-							<EditIcon />
-						</Grid>
-					</button>
-				</td>
-			)}
-
 			{!adminsView && <td>{_id}</td>}
 			<td>
 				<button
-					className="delete-user"
+					className="delete-user delete-icon btn"
 					onClick={handleDeleteUser}
-					style={{ all: 'unset' }}
+					// style={{ all: 'unset' }}
 				>
 					<Grid x={1}>
 						<DeleteIcon />
