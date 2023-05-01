f09b3606f2d43ffa06097000ee9f602b88184094
mahmoudElali <mahmud.elali98@gmail.com>
2023-05-02 01:45:43 +0300

    applied gridding system to available home data

--- src/App.js
+++ src/App.js
@@ -4,28 +4,28 @@ import React, { useState, useEffect } from 'react';
 
 // components
 
-import Home from "./pages/home/Home.jsx";
-import Categories from "./pages/category/Categories.jsx";
-import SubCategories from "./pages/category/SubCategories.jsx";
-import ContactUs from "./pages/contact-us/Contact-us.jsx";
-import Instruction from "./pages/instruction/Instruction.jsx";
-import Orders from "./pages/order/Orders.jsx";
-import Dashboard from "./pages/Dashboard/Dashboard.jsx";
-import Users from "./pages/Dashboard/Users.jsx";
-import Admins from "./pages/Dashboard/Admins.jsx";
-import Products from "./pages/Dashboard/Products.jsx";
-import Settings from "./pages/Dashboard/Settings.jsx";
-import AboutUs from "./pages/about-us/About-us.jsx";
-import Cart from "./pages/cart/Cart.jsx";
-import DashHome from "./pages/Dashboard/DashHome";
-import Pending from "./pages/Dashboard/Pending.jsx";
-import Register from "./components/register/Register";
-import Login from "./components/login/Login";
-import axios from "axios";
-import cookie from "react-cookies";
-import Main from "./pages/main/Main";
-import VisitorUnauth from "./components/visitorUnauth/VisitorUnauth";
-import ProfilePage from "./pages/Profile/Profile.jsx";
+import Home from './pages/home/Home.jsx';
+import Categories from './pages/category/Categories.jsx';
+import SubCategories from './pages/category/SubCategories.jsx';
+import ContactUs from './pages/contact-us/Contact-us.jsx';
+import Instruction from './pages/instruction/Instruction.jsx';
+import Orders from './pages/order/Orders.jsx';
+import Dashboard from './pages/Dashboard/Dashboard.jsx';
+import Users from './pages/Dashboard/Users.jsx';
+import Admins from './pages/Dashboard/Admins.jsx';
+import Products from './pages/Dashboard/Products.jsx';
+import Settings from './pages/Dashboard/Settings.jsx';
+import AboutUs from './pages/about-us/About-us.jsx';
+import Cart from './pages/cart/Cart.jsx';
+import DashHome from './pages/Dashboard/DashHome';
+import Pending from './pages/Dashboard/Pending.jsx';
+import Register from './components/register/Register';
+import Login from './components/login/Login';
+import axios from 'axios';
+import cookie from 'react-cookies';
+import Main from './pages/main/Main';
+import VisitorUnauth from './components/visitorUnauth/VisitorUnauth';
+import ProfilePage from './pages/Profile/Profile.jsx';
 
 export const SidebarStatus = React.createContext();
 export const isLoggedIn = React.createContext();
@@ -46,9 +46,10 @@ function App() {
 			.then((response) => {
 				if (response.status === 200) {
 					if (
-						cookie.load("user").role === "admin" ||
-						cookie.load("user").role === "superAdmin"
+						cookie.load('user').role === 'admin' ||
+						cookie.load('user').role === 'superAdmin'
 					) {
+						cookie.save('username', response.data.message.username);
 						setIsAdmin(true);
 					} else {
 						setIsAdmin(false);
@@ -68,7 +69,6 @@ function App() {
 	useEffect(() => {
 		userIsLoggedIn();
 	}, [loggedIn, isAdmin]);
-
 
 	return (
 		<div className="App">
@@ -105,14 +105,20 @@ function App() {
 									element={<Instruction />}
 								/>
 								<Route path="/orders" element={<Orders />} />
-								<Route path="/profile" element={<ProfilePage />} />
+								<Route
+									path="/profile"
+									element={<ProfilePage />}
+								/>
 								<Route path="/cart" element={<Cart />} />
 							</Route>
 
 							{/* Dashboard routes */}
 
 							{isAdmin && (
-								<Route path="/admin/dashboard" element={<Dashboard />}>
+								<Route
+									path="/admin/dashboard"
+									element={<Dashboard />}
+								>
 									<Route
 										exact
 										path="/admin/dashboard/"
@@ -131,6 +137,10 @@ function App() {
 										element={<Admins />}
 									></Route>
 									<Route
+										path="/admin/dashboard/orders"
+										element={<Orders />}
+									></Route>
+									<Route
 										path="/admin/dashboard/users"
 										element={<Users />}
 									></Route>
@@ -141,7 +151,6 @@ function App() {
 								</Route>
 							)}
 							<Route path="*" element={<VisitorUnauth />} />
-
 						</Routes>
 					</BrowserRouter>
 				</SidebarStatus.Provider>
--- src/pages/Dashboard/DashHome.jsx
+++ src/pages/Dashboard/DashHome.jsx
@@ -12,6 +12,7 @@ const DashHome = () => {
 	const nodeEnv = process.env.REACT_APP_URL;
 	const [products, setProducts] = useState(null);
 	const [users, setUsers] = useState(null);
+	const [orders, setOrders] = useState(null);
 
 	const getAllAdmins = () => {
 		axios
@@ -42,84 +43,136 @@ const DashHome = () => {
 				console.log(err.message);
 			});
 	};
+	const getAllOrders = () => {
+		axios
+			.get(`${nodeEnv}/order`, {
+				headers: {
+					auth_token: Cookies.get('auth_token'),
+				},
+			})
+			.then((res) => {
+				setOrders(res.data.docs);
+				// console.log(res.data.docs);
+			})
+			.catch((err) => {
+				console.log(err.message);
+			});
+	};
 
 	useEffect(getAllProducts, []);
 	useEffect(getAllAdmins, []);
+	useEffect(getAllOrders, []);
 
 	return !products || !users ? (
 		<Loader />
 	) : (
 		<div className="dash-home-container">
 			<h1 className="center">Khizana Dashboard</h1>
-			<div className="products-home-section">
-				<article className="total-products center">
-					<p className="center big-font">
-						{products && products.length}
-					</p>
-					<NavLink to="/admin/dashboard/products">Products</NavLink>
-				</article>
+			<div className="home-grid-container">
+				<div className="products-home-section">
+					<article className="total-products center">
+						<p className="center big-font">
+							{products && products.length}
+						</p>
+						<NavLink to="/admin/dashboard/products">
+							Total <br /> Products
+						</NavLink>
+					</article>
 
-				<article className="onpage-products center">
-					<p className="center big-font">
-						{products &&
-							products.filter((prod) => {
-								return prod.onPage;
-							}).length}
-					</p>
-					<NavLink to="/admin/dashboard/products">Visible</NavLink>
-				</article>
-				<article className="hidden-products center ">
-					<p className="center big-font">
-						{products &&
-							products.filter((prod) => {
-								return !prod.onPage;
-							}).length}
-					</p>
-					<NavLink to="/admin/dashboard/products">Hidden</NavLink>
-				</article>
-			</div>
-			<div className="users-home-section">
-				<article>
-					<p className="center" style={{ fontSize: '2rem' }}>
-						{
-							users.filter((user) => {
-								return user.role === 'user';
-							}).length
-						}
-					</p>
-					<p className="center">
-						<Grid>
-							<PersonIcon
-								sx={{
-									fontSize: '6rem',
-								}}
-							/>
-						</Grid>
-					</p>
-					<div className="center">
-						<NavLink to="/admin/dashboard/users">Users</NavLink>
-					</div>
-				</article>
-				<article>
-					<p className="center" style={{ fontSize: '2rem' }}>
-						{
-							users.filter((user) => {
-								return (
-									user.role === 'admin' ||
-									user.role === 'superAdmin'
-								);
-							}).length
-						}
-					</p>
-					<p className="center">
-						<Grid x={1}>
-							<AdminPanelSettingsIcon sx={{ fontSize: '6rem' }} />
-						</Grid>
-					</p>
-					<p className="center">
-						<NavLink to="/admin/dashboard/admins">Admins</NavLink>
-					</p>
-				</article>
+					<article className="onpage-products center">
+						<p className="center big-font">
+							{orders && orders.length}
+						</p>
+						<NavLink to="/admin/dashboard/orders">
+							Active <br /> Orders
+						</NavLink>
+					</article>
+					<article className="onpage-products center">
+						<p className="center big-font">
+							{products &&
+								products.filter((prod) => {
+									return prod.onPage;
+								}).length}
+						</p>
+						<NavLink to="/admin/dashboard/products">
+							Visible <br /> Products
+						</NavLink>
+					</article>
+					<article className="hidden-products center ">
+						<p className="center big-font">
+							{products &&
+								products.filter((prod) => {
+									return !prod.onPage;
+								}).length}
+						</p>
+						<NavLink to="/admin/dashboard/products">
+							Hidden <br /> Products
+						</NavLink>
+					</article>
+				</div>
+				<div className="users-home-section">
+					<article className="flex-card">
+						<p
+							className="center "
+							style={{
+								fontSize: '4rem',
+								fontFamily: 'Arial Narrow',
+								fontWeight: 900,
+							}}
+						>
+							{
+								users.filter((user) => {
+									return user.role === 'user';
+								}).length
+							}
+						</p>
+						<p className="center">
+							<Grid>
+								<PersonIcon
+									sx={{
+										fontSize: '6rem',
+									}}
+								/>
+							</Grid>
+						</p>
+						<div className="center">
+							<NavLink to="/admin/dashboard/users">
+								Khizana <br /> User
+							</NavLink>
+						</div>
+					</article>
+					<article className="flex-card">
+						<p
+							className="center"
+							style={{
+								fontSize: '4rem',
+								fontFamily: 'Arial Narrow',
+								fontWeight: 900,
+							}}
+						>
+							{
+								users.filter((user) => {
+									return (
+										user.role === 'admin' ||
+										user.role === 'superAdmin'
+									);
+								}).length
+							}
+						</p>
+						<p className="center">
+							<Grid x={1}>
+								<AdminPanelSettingsIcon
+									sx={{ fontSize: '6rem' }}
+								/>
+							</Grid>
+						</p>
+						<p className="center">
+							<NavLink to="/admin/dashboard/admins">
+								Khizana <br /> Admin
+							</NavLink>
+						</p>
+					</article>
+				</div>
 			</div>
 		</div>
 	);
